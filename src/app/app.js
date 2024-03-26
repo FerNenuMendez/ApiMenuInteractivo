de9eb7e57ce2dc7254184637d30a1e9e2c4548e8
Fernando Mendez <fernando.modesto.mendez@gmail.com>
2024-03-26 01:12:30 -0300

    agregado de cors

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,19 @@
 import express from 'express'
-
+import cors from 'cors';
+import { apiRouter } from '../routers/api/api.router.js'
 
 export const app = express()
 
+const corsOptions = {
+    origin: '*', // 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: false,
+    optionsSuccessStatus: 200,
+};
 
+app.use(cors(corsOptions));
+app.use('/api', apiRouter)
+
+app.get('/', (req, res) => {
+    res.send('Api Funcionando OK')
+})
