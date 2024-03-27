a6c435e9f7263c52bf426e14a213c7c1eb5bcc9f
Fernando Mendez <fernando.modesto.mendez@gmail.com>
2024-03-27 20:27:45 -0300

    cambios de vercel.json

--- src/routers/web/web.router.js
+++ src/routers/web/web.router.js
@@ -3,5 +3,5 @@ import { Router } from 'express'
 export const webRouter = Router()
 
 webRouter.get('/', (req, res) => {
-    res.sendFile('index.html', { root: './index.html' })
+    res.sendFile('index.html', { root: 'index.html' })
 })
