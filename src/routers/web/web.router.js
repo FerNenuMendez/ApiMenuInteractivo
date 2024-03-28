00a73d15a620c7b3d852504afc36f6857cd994d4
Fernando Mendez <fernando.modesto.mendez@gmail.com>
2024-03-27 21:37:35 -0300

    cambio de vercel.json

--- src/routers/web/web.router.js
+++ src/routers/web/web.router.js
@@ -1,20 +1,11 @@
 import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 export const webRouter = express.Router();
 
 // Ruta principal que sirve el archivo index.html
 webRouter.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../index.html'));
+    res.sendFile('../../index.html');
 });
-
-// Ruta para servir archivos estáticos como CSS, JS, imágenes, etc.
-webRouter.use('/static', express.static(path.join(__dirname, '../../static')));
 
 // Cualquier otra ruta que necesites para tu web
 webRouter.get('/test', (req, res) => {
