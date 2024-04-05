d985284365f7165a2122ab81fccc03b5aad73339
Fernando Mendez <fernando.modesto.mendez@gmail.com>
2024-04-04 21:54:01 -0300

    agregado de js

--- js/index.js
+++ js/index.js
@@ -1,2 +1,17 @@
 const carSec = document.getElementById('carrousel__section')
+const verMenuBtn = document.getElementById('verMenuBtn')
 
+verMenuBtn.addEventListener('click', function () {
+    var nombre = document.getElementById("nombre").value;
+    var mesa = document.getElementById("mesa").value;
+    if (nombre.length >= 3 && mesa.length >= 3) {
+        verMenu();
+    } else {
+        alert("Por favor, ingrese al menos 3 caracteres en ambos campos.");
+    }
+});
+
+function verMenu() {
+    // Tu lógica para mostrar el menú aquí
+    alert("¡Mostrando el menú!");
+}
