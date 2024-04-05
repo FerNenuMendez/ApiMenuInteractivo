2ec0b8f5c93793c7a4fee5c3f3768dd2e802e934
Fernando Mendez <fernando.modesto.mendez@gmail.com>
2024-04-04 23:20:50 -0300

    agregado de menu

--- js/index.js
+++ js/index.js
@@ -12,6 +12,46 @@ verMenuBtn.addEventListener('click', function () {
 });
 
 function verMenu() {
-    // Tu lógica para mostrar el menú aquí
-    alert("¡Mostrando el menú!");
+    fetch('https://api-menu-six.vercel.app/api/productos/')
+        .then(response => response.json())
+        .then(data => {
+            carSec.innerHTML = ""; // Limpiar contenido anterior
+
+            if (data.status === "success") {
+                const productos = data.payload;
+
+                productos.forEach(producto => {
+                    const card = document.createElement("div");
+                    card.classList.add("card");
+
+                    const imagen = document.createElement("img");
+                    imagen.classList.add("card__img");
+                    imagen.src = producto.foto;
+                    imagen.alt = producto.nombre;
+                    card.appendChild(imagen);
+
+                    const nombre = document.createElement("h3");
+                    nombre.classList.add("card__nombre");
+                    nombre.textContent = producto.nombre;
+                    card.appendChild(nombre);
+
+                    const detalle = document.createElement("p");
+                    detalle.classList.add("card__detalle");
+                    detalle.textContent = producto.detalle;
+                    card.appendChild(detalle);
+
+                    const precio = document.createElement("p");
+                    precio.classList.add("card__precio");
+                    precio.textContent = `Precio: ${producto.precio}`;
+                    card.appendChild(precio);
+
+                    carSec.appendChild(card);
+                });
+            } else {
+                console.error('Error en la respuesta del servidor:', data);
+            }
+        })
+        .catch(error => {
+            console.error('Error al obtener los productos:', error);
+        });
 }
