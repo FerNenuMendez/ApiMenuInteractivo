384141900e953cf8fcf6e2d3e75e872a90f57618
Fernando Mendez <fernando.modesto.mendez@gmail.com>
2024-05-06 22:43:45 -0300

    cambios de orden de rutas

--- src/routers/api/bebidas.router.js
+++ src/routers/api/bebidas.router.js
@@ -2,13 +2,11 @@ import { Router } from 'express'
 import { getController, getByIdController, postController, deleteController } from '../../controllers/bebidas.controller.js'
 
 export const bebidasRouter = Router()
-
-bebidasRouter.get('/', getController)
-bebidasRouter.get('/:id', getByIdController)
-bebidasRouter.post('/', postController)
-bebidasRouter.delete('/:id', deleteController)
-
-
 bebidasRouter.get('/test', (req, res) => {
     res.status(200).send('Bebidas Router OK')
 })
+bebidasRouter.get('/:id', getByIdController)
+bebidasRouter.get('/', getController)
+bebidasRouter.post('/', postController)
+bebidasRouter.delete('/:id', deleteController)
+
--- src/routers/api/productos.router.js
+++ src/routers/api/productos.router.js
@@ -2,12 +2,11 @@ import { Router } from 'express'
 import { getController, getByIdController, postController, deleteController } from '../../controllers/productos.controller.js'
 
 export const productosRouter = Router()
-
-productosRouter.get('/', getController)
-productosRouter.get('/:id', getByIdController)
-productosRouter.post('/', postController)
-productosRouter.delete('/:id', deleteController)
-
 productosRouter.get('/test', (req, res) => {
     res.status(200).send('Productos Router OK')
 })
+productosRouter.get('/:id', getByIdController)
+productosRouter.get('/', getController)
+productosRouter.post('/', postController)
+productosRouter.delete('/:id', deleteController)
+
